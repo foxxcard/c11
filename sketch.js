19fccd957820b1e0aefead9d12ebb4a2ae4b8397
foxxcard <109637863+foxxcard@users.noreply.github.com>
2022-08-23 20:04:23 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+var trex ,trex_running;
+function preload(){
+ trex_running=loadAnimation ("trex1.png","trex3.png","trex4.png")
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex = createSprite (50,100,60,40)
+ trex.addAnimation("running",trex_running)
+}
+function draw(){
+  background("pink")
+  
+drawSprites ()
+} 
